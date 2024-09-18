7288d67e3258201bcac1e3e9e90f68a57521acf5
sphamel <159089332+sphamel@users.noreply.github.com>
2024-09-18 21:30:23 +0000

    Updated lab02

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Sawyer Hamel
 # UWYO COSC 1010
-# Submission Date
+# 09/18/2024
 # Lab 02 
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 15
+# Sources, people worked with, help given to: NA
 # your
 # comments
 # here
@@ -13,20 +13,22 @@ your_variable_here = "when you see this, replace it with your code"
 ## Section ONE
 
 # Complete the following print statement to print out "Hello, COSC1010"
-print()
+print("Hello, COSC1010")
 
 # Assign the string above to a variable named hello_message and print that variable
-hello_message = ""
-print("Your code here")
+hello_message = "Hello, COSC1010"
+print(hello_message)
 
 # Assign the string "cowboy joe" to a variable, and print that variable with title casing
-print("Your code here")
+pony = "cowboy joe"
+print(pony.title())
 
 # Complete the following f-string print message 
     # You will need to create your own variables and insert them  
     # the final message should read `The University of Wyoming was founded in 1886`
-
-print(f"The {your_variable_here} was founded in {your_variable_here}")
+institution = "University of Wyoming"
+year = 1886
+print(f"The {institution} was founded in {year}")
 
 # Now let's do some math with variables 
     # Create two variables x and y and assign them the values 5 and 10 respectively 
